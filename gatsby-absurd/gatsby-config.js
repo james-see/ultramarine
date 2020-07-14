ee931797c35e9fa5c863b59954745e7c1e35316e
James Campbell <james@jamescampbell.us>
2020-07-13 23:59:48 -0400

    updates

--- gatsby-absurd/gatsby-config.js
+++ gatsby-absurd/gatsby-config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  pathPrefix: `/ultramarine/gatsby-absurd`,
+  pathPrefix: `/ultramarine`,
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
